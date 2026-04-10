d005d62103199499966534f3f3dde23db47d4ac2
Kauê Nascimento <kaue.sc78@gmail.com>
2026-04-10 16:50:10 -0300

    feat: adicionar banco de dados NeonDB para produção e SQLite para local

--- SAVO/SAVO/settings.py
+++ SAVO/SAVO/settings.py
@@ -55,13 +55,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'SAVO.wsgi.application'
 
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('ENVIRONMENT') == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
 
 
 
